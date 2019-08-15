beb7c2ccbbef1c560ef241ca6bf4fe95404ced6e
HelaudyB <e.belguet@gmail.com>
2019-08-15 20:46:42 +0200

    ajout d'une div animée

--- script.js
+++ script.js
@@ -123,3 +123,46 @@ $(document).ready(function(){
   });
   
 });
+
+var m = document.querySelector('.main'); 
+var mail = document.querySelector('.mail'); 
+var b = document.querySelector('.button'); 
+var w = window.innerWidth;
+var h = window.innerHeight;
+var bw = document.querySelectorAll('.bw');
+document.body.addEventListener('mousemove',move);
+var p;
+
+if(w>1024){
+  p=64;
+}else{
+ p=32;
+}
+
+
+
+function move(e){
+  var rgb = [
+      Math.round(e.pageX/w * 360),
+      Math.round(e.pageY/h * 90)+'%',
+      '40%'
+  ];
+  
+  
+  m.style.backgroundColor = 'hsl('+rgb.join(',')+')';
+  b.style.color = 'hsl('+rgb.join(',')+')';
+
+  document.body.style.backgroundColor = 'hsla('+rgb.join(',')+',.1)';
+
+  for(var i=0;i<bw.length;i++){
+    if(bw[i].classList.length===1){
+      bw[i].style.backgroundColor = 'hsla('+rgb.join(',')+',.1)';
+    }
+    bw[i].style.transform = ' translate('+(0.5-e.clientX/(w-16))*-p+'px,'+(0.5-e.clientY/h)*-p+'px)';
+  }
+
+}
+var regex = /\+/gi;
+var less = mail.getAttribute('href');
+mail.setAttribute('href',less.replace(regex, ''));
+mail.textContent = less.replace(regex, '').substr(7);
